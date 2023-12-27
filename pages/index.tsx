b9fb2c98f9168ade483cfdb15fa660f04eb76aba
Christopher Steinagel <gabriel.steinagel@agrodatai.com>
2023-12-27 00:40:08 -0500

    feat: ShopLayout and Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Typography } from '@mui/material'
+import { ShopLayout } from '../components/layouts'
 
 export default function Home() {
   return (
-    <>
-      <h1>Ecommerce Adso</h1>
-    </>
+    <ShopLayout title={'Ecommerce Sena - Home'} pageDescription={'Encuentra los mejores artículos para tu setup'}>
+      <Typography variant='h1' component={'h1'}>Tienda</Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>Artículos</Typography>
+    </ShopLayout>
   )
 }
