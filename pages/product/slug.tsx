ca5da73bba56ece7d0995760551f83570db9848d
Christopher Steinagel <gabriel.steinagel@agrodatai.com>
2024-01-02 00:04:14 -0500

    Add: slug

---
+++ pages/product/slug.tsx
@@ -0,0 +1,45 @@
+import { Box, Button, Grid, Typography, Chip } from "@mui/material"
+import { ShopLayout } from "../../components/layouts"
+import { initialData } from "../../database/products"
+import { ProductSlidesShow } from "../../components/products/ProductSlidesShow"
+import { SizeSelector } from "../../components/products/SizeSelector"
+import { ItemCounter } from "../../components/ui/ItemCounter"
+
+const product = initialData.products[0]
+
+const ProductPage = () => {
+    return (
+        <ShopLayout title={product.title} pageDescription={product.description}>
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={7}>
+                    <ProductSlidesShow images={product.images} />
+                </Grid>
+                <Grid item xs = {12} sm={5}>
+                    <Box display='flex' flexDirection='column'>
+                        {/* títulos */}
+                        <Typography variant='h1' component='h1'>{product.title}</Typography>
+                        <Typography variant='subtitle1' component='h2'>{`$${product.price}`}</Typography>
+                        {/* cantidad */}
+                        <Box sx={{my: 2}} >
+                            <Typography variant="subtitle2">Cantidad</Typography>
+                            {/* itemCounter */}
+                            <ItemCounter />
+                            <SizeSelector sizes={product.sizes} />
+                        </Box>
+                        <Button color="secondary" className="circular-btn">
+                            Agregar a carrito
+                        </Button>
+                        {/* <Chip label='No hay disponibles' color='error' variant='outlined' /> */}
+                        {/* Descripción */}
+                        <Box sx={{mt: 3}}>
+                            <Typography variant="subtitle2">Descripción</Typography>
+                            <Typography variant="body2">{product.description}</Typography>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </ShopLayout>
+    )
+}
+
+export default ProductPage
