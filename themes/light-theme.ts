04df85b58526787cc155697cceeff871484206df
Christopher Steinagel <gabriel.steinagel@agrodatai.com>
2024-01-01 23:05:26 -0500

    Change

--- themes/light-theme.ts
+++ themes/light-theme.ts
@@ -1,6 +1,5 @@
 import { createTheme } from '@mui/material/styles';
-import { red,  } from '@mui/material/colors';
-
+import { red } from '@mui/material/colors';
 
 export const lightTheme = createTheme({
   palette: {
@@ -9,7 +8,10 @@ export const lightTheme = createTheme({
       main: '#1E1E1E'
     },
     secondary: {
-      main: '#3A64D8'
+      main: '#30d680'
+    },
+    info: {
+      main: '#fff'
     }
   },
   components: {
@@ -48,17 +50,15 @@ export const lightTheme = createTheme({
       }
     },
 
-
     MuiButton: {
       defaultProps: {
         variant: 'contained',
         size: 'small',
         disableElevation: true,
+        color: 'info'
       },
       styleOverrides: {
         root: {
-          backgroundColor: 'white',
-          color: 'black',
           textTransform: 'none',
           boxShadow: 'none',
           borderRadius: 10,
@@ -69,7 +69,6 @@ export const lightTheme = createTheme({
         }
       }
     },
-
 
     MuiCard: {
       defaultProps: {
